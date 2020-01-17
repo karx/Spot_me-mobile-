3549b44316c72fbefffe9bba7e0f1c74048e28e4
karx <kartik.arora1214@gmail.com>
2020-01-18 02:23:00 +0530

    formatting and name

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-var number= Math.floor(Math.random()*8888)+1111; 
-document.getElementById('random_number').innerHTML=number+"please input this number in your spot me devise";
-function perform_vibration()
-{
-if(servercall==1)
-{
-window.navigator.vibrate(1000);
-} else if(servercall==0)
-{
-window.navigator.vibrate([1000, 1000, 1000]);
-} else {
-document.write('value is null');
-}
+var number = Math.floor(Math.random() * 8888) + 1111;
+document.getElementById('connection_code').innerHTML = number + "please input this number in your spot me devise";
+function perform_vibration() {
+    if (servercall == 1) {
+        window.navigator.vibrate(1000);
+    } else if (servercall == 0) {
+        window.navigator.vibrate([1000, 1000, 1000]);
+    } else {
+        document.write('value is null');
+    }
 }
