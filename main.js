67d3d2678b97cdd23234850d53e3379e7d05eab5
kulwinder96 <33957493+kulwinder96@users.noreply.github.com>
2020-01-18 02:10:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var number= Math.floor(Math.random()*8888)+1111; 
+document.getElementById('random_number').innerHTML=number+"please input this number in your spot me devise";
+function perform_vibration()
+{
+if(servercall==1)
+{
+window.navigator.vibrate(1000);
+} else if(servercall==0)
+{
+window.navigator.vibrate([1000, 1000, 1000]);
+} else {
+document.write('value is null');
+}
+}
